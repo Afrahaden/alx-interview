65d6d848fc5dd9a99e390d6d415434fb4f8cd714
Afrah Aden <afrahaden05@gmail.com>
2023-09-25 08:46:41 -0700

    Pascal's Triangle

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+This module defines the function 'pascal_triangle'
+"""
+
+
+def pascal_triangle(n):
+        """
+            Returns a list of lists of integers representing the Pascal's triangle of n
+                """
+                    if n <= 0:
+                                return []
+                                pasca = [[1]]
+                                    for i in range(1, n):
+                                                row = [1]
+                                                        for j in range(1, i):
+                                                                        if i - 1 == 0:
+                                                                                            break
+                                                                                                    row.append(pasca[i - 1][j - 1] + pasca[i - 1][j])
+                                                                                                            row.append(1)
+                                                                                                                    pasca.append(row)
+
+                                                                                                                        return pasca
