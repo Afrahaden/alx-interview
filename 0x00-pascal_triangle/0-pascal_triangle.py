5e92f4a3c3353124502895a112160df1f8287f25
Afrah Aden <afrahaden05@gmail.com>
2023-09-27 00:57:10 -0700

    Pascal's Triangle

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,23 +1,24 @@
 #!/usr/bin/python3
 """
-This module defines the function 'pascal_triangle'
+0-pascal_triangle
 """
 
 
 def pascal_triangle(n):
         """
-            Returns a list of lists of integers representing the Pascal's triangle of n
-                """
-                    if n <= 0:
-                                return []
-                                pasca = [[1]]
-                                    for i in range(1, n):
-                                                row = [1]
-                                                        for j in range(1, i):
-                                                                        if i - 1 == 0:
-                                                                                            break
-                                                                                                    row.append(pasca[i - 1][j - 1] + pasca[i - 1][j])
-                                                                                                            row.append(1)
-                                                                                                                    pasca.append(row)
-
-                                                                                                                        return pasca
+            Returns a list of integers
+                representing the Pascal Triangle of n
+                    returns empty list if n <= 0
+                        """
+                            k = []
+                                if n <= 0:
+                                            return k
+                                            k = [[1]]
+                                                for i in range(1, n):
+                                                            temp = [1]
+                                                                    for j in range(len(k[i - 1]) - 1):
+                                                                                    curr = k[i - 1]
+                                                                                                temp.append(k[i - 1][j] + k[i - 1][j + 1])
+                                                                                                        temp.append(1)
+                                                                                                                k.append(temp)
+                                                                                                                    return k
