97f79b1b0c7c853cec18bdcede4b9ce2595e3622
Afrah Aden <afrahaden05@gmail.com>
2023-10-03 15:42:01 -0700

    update

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,15 +1,20 @@
-#!/usr/bin/python3
+
+dule for working with lockboxes.
+"""
+
 def canUnlockAll(boxes):
-        if type(boxes) is not list:
-                    return False
-                    elif (len(boxes)) == 0:
-                                return False
-                                for k in range(1, len(boxes) - 1):
-                                            boxes_checked = False
-                                                    for idx in range(len(boxes)):
-                                                                    boxes_checked = k in boxes[idx] and k != idx
-                                                                                if boxes_checked:
-                                                                                                    break
-                                                                                                        if boxes_checked is False:
-                                                                                                                        return boxes_checked
-                                                                                                                        return True
+        posn = 0
+            unlocked = {}
+
+                for index in range(len(boxes)):
+
+                        if len(boxes[index]) == 0 or index == 0:
+                                    unlocked[index] = "unlocked"
+
+                                            for key in boxes[index]:
+                                                        if key < len(boxes) and key != index:
+                                                                        unlocked[key] = key
+
+                                                                                if len(unlocked) == len(boxes):
+                                                                                            return True
+                                                                                                return False
